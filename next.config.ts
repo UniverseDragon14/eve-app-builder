eb211f95e85beee4c6a1e4e3f36eab9a454b56e0
Aslam <univercialdragon@gmail.com>
2026-05-06 13:54:10 +0400

    Fix EVE button API and dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "192.168.70.117",
+    "192.168.70.119",
+    "nova-pi.local",
+    "localhost",
+  ],
+};
 
 export default nextConfig;
